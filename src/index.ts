6e7f554112e7bbf9863db84387912a16e1528301
ShiCheng Lu <shichenglu0708@gmail.com>
2022-01-15 21:59:14 -0500

    corrected export, updated doc

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * from "./jomlmatrix4.js";
 export * from "./jomlvector3.js";
 export * from "./jomlvector4.js";
-export * from "./vector2.js";
+export * from "./jomlvector2.js";
 
 // export * from './vector2.js'
 // export * from './vector3.js'
